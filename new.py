c5098ef9e51d2a1c99ef9884c3e8559042fd1388
Franco <168871500+SrFluff@users.noreply.github.com>
2024-05-16 18:57:21 -0400

    Renamed to PyShell, many bugs

--- new.py
+++ new.py
@@ -3,3 +3,82 @@ import os
 def cls():
 
     os.sytem('clear')
+
+runtime = 1
+
+start = 1
+
+lock = 0
+
+lockfail = 0
+
+password = ""
+
+while runtime:
+
+    while start:
+
+        cls()
+
+        print("PyShell lite\n")
+
+        print("b - boot")
+        print("s - settings")
+        print("p - set password")
+        print("l - lock")
+        print("q - quit\n")
+
+        a = input("> ")
+
+        if a == "q":
+
+            cls()
+
+            quit()
+
+        if a == "l":
+
+            if password != "":
+
+                start = 0
+
+                lock = 1
+
+            else:
+
+                start = 0
+
+                lockfail = 1
+
+    while lock:
+
+        cls()
+
+        print("PyShell lite\n")
+
+        print("a - enter password")
+        print("q - quit\n")
+
+        a = input("> ")
+
+        if a == "a":
+
+            cls()
+
+            print("PyShell lite\n")
+
+            print("Enter the password\n")
+
+            a = input("> ")
+
+            if a == password:
+
+                lock = 0
+
+                start = 1
+
+            else:
+
+                cls()
+
+                lock = 1
